21b2c915126f08586bd9a81581ffb0197bf81475
Kaida-27 <157682708+Kaida-27@users.noreply.github.com>
2024-01-27 11:03:12 -0500

    Update build.sh

--- KABI-Calamares-Config/build.sh
+++ KABI-Calamares-Config/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+SCRIPT_DIR2="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+cd $SCRIPT_DIR2
+
 destiny=$SCRIPT_DIR"/build/"
 search="KABI-calamares-config"
 
